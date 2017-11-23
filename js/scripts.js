a015b353e2d400c72d1536fd14c5e11576f6a150
Arnold Sztandarski <a.sztandarski@wp.pl>
2017-11-23 09:28:27 +0100

    Add scripts

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('Enter your the side length of the triangle');
+	h = prompt('Enter your the height of the triangle');
+	triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
